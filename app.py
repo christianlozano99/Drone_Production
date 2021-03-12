53a8217ccc4f66885329f218b6241ab56a7c404e
Christian Lozano <christianlozano@MacBook-Pro.local>
2021-03-12 10:18:31 -0500

    setting up new git

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/coordinateInput")
+def coordinateInput():
+    return render_template("coordniatePage.html")
+
+@app.route("/controlOverride")
+def overrideControls():
+    return render_template("controlOverride.html")
+
+if __name__ == "__main__":
+    app.run(host='10.32.195.100',debug = True,threaded=True)
